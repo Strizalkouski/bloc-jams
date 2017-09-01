d0223535334c77f1f0a136d2b331d746885bc03f
Mark Strizalkouski <markstrizalkouski@gmail.com>
2017-09-01 15:56:04 -0400

    changes based on mentor conversation

--- scripts/album.js
+++ scripts/album.js
@@ -125,13 +125,8 @@ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause">
 var currentlyPlayingSong = null;
 
 $(document).ready(function() {
-   var $albumImage = $('.album-cover-art');
     setCurrentAlbum(albumPicasso);
-    for (var i = 0; i <songRows.length; i++){
-        songRows[i].addEventListener('click', function(event) {
-             clickHandler(event.target);
-        });
-    }
+   
 
     var albums = [albumPicasso, albumMarconi, albumDracarys];
     var index = 1;
@@ -142,5 +137,5 @@ $(document).ready(function() {
             index = 0;
         }
     });
-});
+
 
